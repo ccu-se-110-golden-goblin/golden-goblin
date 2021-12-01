6a069da09f6bcdc831e210150cd5511254328bc3
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-12-01 20:40:19 +0800

    [UI] Update theme data.

--- lib/src/themes.dart
+++ lib/src/themes.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 class GoldenGoblinThemes {
-  static ThemeData get light => ThemeData.light().copyWith();
+  static AppBarTheme get _customAppBarTheme => ThemeData().appBarTheme.copyWith(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(25),
+          ),
+        ),
+        foregroundColor: Colors.black,
+        backgroundColor: const Color(0xFFFFD344),
+        elevation: 0.0,
+      );
 
-  static ThemeData get dark => ThemeData.dark().copyWith();
+  static ThemeData get light => ThemeData.light().copyWith(
+        appBarTheme: _customAppBarTheme,
+        primaryColor: const Color(0xFFFFD344),
+      );
+
+  static ThemeData get dark => ThemeData.dark().copyWith(
+        appBarTheme: _customAppBarTheme,
+        primaryColor: const Color(0xFFFFD344),
+      );
 }
