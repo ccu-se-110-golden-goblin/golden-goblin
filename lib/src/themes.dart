d1154ea800bfba3405f2ae8508038ecd56ca9645
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2021-12-07 00:24:34 +0800

    [modify] remove rounded appBar

--- lib/src/themes.dart
+++ lib/src/themes.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 
 class GoldenGoblinThemes {
   static AppBarTheme get _customAppBarTheme => ThemeData().appBarTheme.copyWith(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(25),
-          ),
-        ),
         foregroundColor: Colors.black,
         backgroundColor: const Color(0xFFFFD344),
         elevation: 0.0,
