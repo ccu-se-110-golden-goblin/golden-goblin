5f702d62b5aa16da70a76f678547f4aa3f1ef1de
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-12-09 20:33:54 +0800

    [Modify] Add AccountProvider and CategoryProvider to provider and move Provider up before MyApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'src/models/account_provider.dart';
+import 'src/models/category_provider.dart';
+import 'src/models/transaction_provider.dart';
+import 'src/models/transfer_provider.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
@@ -13,8 +19,30 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  final accountProvider = AccountProvider();
+
+  await accountProvider.loadAccounts();
+
+  final categoryProvider = CategoryProvider();
+
+  await categoryProvider.loadCategories();
+
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(MultiProvider(
+    providers: [
+      Provider.value(value: accountProvider),
+      Provider.value(value: categoryProvider),
+      Provider<TransactionProvider>(
+        create: (_) => TransactionProvider(),
+        lazy: false,
+      ),
+      Provider<TransferProvider>(
+        create: (_) => TransferProvider(),
+        lazy: false,
+      ),
+    ],
+    child: MyApp(settingsController: settingsController),
+  ));
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:golden_goblin/src/views/ledger_view/ledger_edit_view.dart';
-import 'package:provider/provider.dart';
-
-import 'models/transaction_provider.dart';
-import 'models/transfer_provider.dart';
 
 import 'themes.dart';
 import 'views/ledger_view/ledger_view.dart';
@@ -25,79 +21,65 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Glue the SettingsController to the MaterialApp.
     //
-    return MultiProvider(
-      providers: [
-        Provider<TransactionProvider>(
-          create: (_) => TransactionProvider(),
-          lazy: false,
-        ),
-        Provider<TransferProvider>(
-          create: (_) => TransferProvider(),
-          lazy: false,
-        ),
-      ],
-      // The AnimatedBuilder Widget listens to the SettingsController for changes.
-      // Whenever the user updates their settings, the MaterialApp is rebuilt.
-      child: AnimatedBuilder(
-        animation: settingsController,
-        builder: (BuildContext context, Widget? child) {
-          return MaterialApp(
-            // Providing a restorationScopeId allows the Navigator built by the
-            // MaterialApp to restore the navigation stack when a user leaves and
-            // returns to the app after it has been killed while running in the
-            // background.
-            //restorationScopeId: 'app',
+    return AnimatedBuilder(
+      animation: settingsController,
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          // Providing a restorationScopeId allows the Navigator built by the
+          // MaterialApp to restore the navigation stack when a user leaves and
+          // returns to the app after it has been killed while running in the
+          // background.
+          //restorationScopeId: 'app',
 
-            // Provide the generated AppLocalizations to the MaterialApp. This
-            // allows descendant Widgets to display the correct translations
-            // depending on the user's locale.
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: const [
-              Locale('en', ''), // English, no country code
-            ],
+          // Provide the generated AppLocalizations to the MaterialApp. This
+          // allows descendant Widgets to display the correct translations
+          // depending on the user's locale.
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''), // English, no country code
+          ],
 
-            // Use AppLocalizations to configure the correct application title
-            // depending on the user's locale.
-            //
-            // The appTitle is defined in .arb files found in the localization
-            // directory.
-            onGenerateTitle: (BuildContext context) =>
-                AppLocalizations.of(context)!.appTitle,
+          // Use AppLocalizations to configure the correct application title
+          // depending on the user's locale.
+          //
+          // The appTitle is defined in .arb files found in the localization
+          // directory.
+          onGenerateTitle: (BuildContext context) =>
+              AppLocalizations.of(context)!.appTitle,
 
-            // Define a light and dark color theme. Then, read the user's
-            // preferred ThemeMode (light, dark, or system default) from the
-            // SettingsController to display the correct theme.
-            theme: GoldenGoblinThemes.light,
-            darkTheme: GoldenGoblinThemes.dark,
-            themeMode: settingsController.themeMode,
+          // Define a light and dark color theme. Then, read the user's
+          // preferred ThemeMode (light, dark, or system default) from the
+          // SettingsController to display the correct theme.
+          theme: GoldenGoblinThemes.light,
+          darkTheme: GoldenGoblinThemes.dark,
+          themeMode: settingsController.themeMode,
 
-            // Define a function to handle named routes in order to support
-            // Flutter web url navigation and deep linking.
-            onGenerateRoute: (RouteSettings routeSettings) {
-              return MaterialPageRoute<void>(
-                settings: routeSettings,
-                builder: (BuildContext context) {
-                  switch (routeSettings.name) {
-                    case SettingsView.routeName:
-                      return SettingsView(controller: settingsController);
-                    case LedgerView.routeName:
-                      return const LedgerView();
-                    case LedgerEditView.routeName:
-                      return const LedgerEditView();
-                    default:
-                      return const LedgerView();
-                  }
-                },
-              );
-            },
-          );
-        },
-      ),
+          // Define a function to handle named routes in order to support
+          // Flutter web url navigation and deep linking.
+          onGenerateRoute: (RouteSettings routeSettings) {
+            return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case SettingsView.routeName:
+                    return SettingsView(controller: settingsController);
+                  case LedgerView.routeName:
+                    return const LedgerView();
+                  case LedgerEditView.routeName:
+                    return const LedgerEditView();
+                  default:
+                    return const LedgerView();
+                }
+              },
+            );
+          },
+        );
+      },
     );
   }
 }
