e541e77425b07cc5723cbcdf6d7a833622e63e62
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-12-15 20:14:06 +0800

    [Impl] Implement CategoryProvider for database storage.

--- lib/src/models/category_provider.dart
+++ lib/src/models/category_provider.dart
@@ -1,17 +1,22 @@
 import 'catagory.dart';
 
+import '../helpers/db_helper.dart';
+
 class CategoryProvider {
   List<Category> _categories = [];
 
   Future<void> loadCategories() async {
-    //TODO: Fetch from DB
-    //mock data
-    _categories = [
-      Category(id: 0, name: "Cat 1", type: Type.income),
-      Category(id: 1, name: "Cat 2", type: Type.income),
-      Category(id: 2, name: "Cat 3", type: Type.income),
-      Category(id: 3, name: "Cat 4", type: Type.expenses),
-    ];
+    var db = await DBHelper.opendb();
+
+    List<Map> rawCategories = await db.query('categories');
+
+    _categories = rawCategories
+        .map((mapobj) => Category(
+              id: mapobj['id'],
+              name: mapobj['name'],
+              type: Type.values[mapobj['type']],
+            ))
+        .toList();
   }
 
   List<Category> get getCategories => _categories;
@@ -23,36 +28,40 @@ class CategoryProvider {
   }
 
   // When insert into database, id will be ignore and replaced, use getAccounts to get new list with new id
-  Future<void> addCategory(Category category) async {
-    //TODO: Add and reload from DB
-    //mock operation
-    _categories.add(Category(
-      id: _categories.last.id + 1,
-      name: category.name,
-      type: category.type,
-    ));
+  Future<int> addCategory(Category category) async {
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    var categoryMap = category.toMap();
+
+    categoryMap.remove('id');
+    categoryMap['type'] = (categoryMap['type'] as Type).index;
+
+    var recordid = await db.insert('categories', categoryMap);
+
+    await loadCategories();
+
+    return recordid;
   }
 
   Future<void> deleteCategory(int categoryId) async {
-    //TODO: delete and reload from DB
-    //mock operation
-    _categories.removeWhere((category) => category.id == categoryId);
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    await db.delete('categories', where: 'id = ?', whereArgs: [categoryId]);
+
+    await loadCategories();
   }
 
   Future<void> updateCategory(int categoryId, Category newCategory) async {
-    //TODO: update and reload from DB
-    //mock operation
-    _categories.removeWhere((category) => category.id == categoryId);
-    _categories.add(Category(
-      id: categoryId,
-      name: newCategory.name,
-      type: newCategory.type,
-    ));
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    var categoryMap = newCategory.toMap();
+
+    categoryMap.remove('id');
+    categoryMap['type'] = (categoryMap['type'] as Type).index;
+
+    await db.update('categories', categoryMap,
+        where: 'id = ?', whereArgs: [categoryId]);
+
+    await loadCategories();
   }
 }
