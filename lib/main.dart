70d5e7ecdcb597419aade229c96321439a760481
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2021-12-15 13:07:26 +0800

    [add] Install firebase and crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'firebase_options.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
-  WidgetsFlutterBinding.ensureInitialized();
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  final settingsController = SettingsController(SettingsService());
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
+    if (kReleaseMode) {
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    }
 
-  await settingsController.loadSettings();
+    final settingsController = SettingsController(SettingsService());
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+    // Load the user's preferred theme while the splash screen is displayed.
+    // This prevents a sudden theme change when the app is first displayed.
+
+    await settingsController.loadSettings();
+
+    // Run the app and pass in the SettingsController. The app listens to the
+    // SettingsController for changes, then passes it further down to the
+    // SettingsView.
+    runApp(MyApp(settingsController: settingsController));
+  },
+      (error, stack) => {
+            if (kReleaseMode)
+              FirebaseCrashlytics.instance.recordError(error, stack)
+          });
 }
