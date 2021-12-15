64dcd72a3a2b74ba7dd442fa9eeff612297242b1
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-12-15 19:39:02 +0800

    [Impl] Implement AccountProvider for database storage.

--- lib/src/models/account_provider.dart
+++ lib/src/models/account_provider.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'account.dart';
+import '../helpers/db_helper.dart';
 
 class AccountProvider {
   List<Account> _accounts = [];
 
   //This is only for init AccountProvider
   Future<void> loadAccounts() async {
-    //TODO: Fetch from DB
-    //mock data
-    _accounts = [
-      Account(
-        id: 0,
-        name: "Account 1",
-        icon: Icons.face_retouching_natural,
-        iconColor: Colors.blue,
-      ),
-      Account(
-        id: 1,
-        name: "Account 2",
-        icon: Icons.hail,
-        iconColor: Colors.orange,
-      ),
-    ];
+    var db = await DBHelper.opendb();
+
+    List<Map> rawAccounts = await db.query('accounts');
+
+    _accounts = rawAccounts
+        .map((mapobj) => Account(
+              id: mapobj['id'],
+              name: mapobj['name'],
+              icon: IconData(mapobj['icon']),
+              iconColor: Color(mapobj['iconcolor']),
+            ))
+        .toList();
   }
 
   List<Account> get getAccounts => _accounts;
@@ -34,38 +31,42 @@ class AccountProvider {
   }
 
   // When insert into database, id will be ignore and replaced, use getAccounts to get new list with new id
-  Future<void> addAccount(Account account) async {
-    //TODO: Add and reload from DB
-    //mock operation
-    _accounts.add(Account(
-      id: _accounts.last.id + 1,
-      name: account.name,
-      icon: account.icon,
-      iconColor: account.iconColor,
-    ));
+  Future<int> addAccount(Account account) async {
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    var accountMap = account.toMap();
+
+    accountMap.remove('id');
+    accountMap['icon'] = (accountMap['icon'] as IconData).codePoint;
+    accountMap['iconColor'] = (accountMap['iconColor'] as Color).value;
+
+    var recordid = await db.insert('accounts', accountMap);
+
+    await loadAccounts();
+
+    return recordid;
   }
 
   Future<void> deleteAccount(int accountId) async {
-    //TODO: delete and reload from DB
-    //mock operation
-    _accounts.removeWhere((account) => account.id == accountId);
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    await db.delete('accounts', where: 'id = ?', whereArgs: [accountId]);
+
+    await loadAccounts();
   }
 
   Future<void> updateAccount(int accountId, Account newAccount) async {
-    //TODO: update and reload from DB
-    //mock operation
-    _accounts.removeWhere((account) => account.id == accountId);
-    _accounts.add(Account(
-      id: accountId,
-      name: newAccount.name,
-      icon: newAccount.icon,
-      iconColor: newAccount.iconColor,
-    ));
+    var db = await DBHelper.opendb();
 
-    await Future.delayed(const Duration(seconds: 3));
+    var accountMap = newAccount.toMap();
+
+    accountMap.remove('id');
+    accountMap['icon'] = (accountMap['icon'] as IconData).codePoint;
+    accountMap['iconColor'] = (accountMap['iconColor'] as Color).value;
+
+    await db.update('accounts', accountMap,
+        where: 'id = ?', whereArgs: [accountId]);
+
+    await loadAccounts();
   }
 }
