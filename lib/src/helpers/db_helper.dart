93e5086a20c57cacfd98f601573344600b529f27
Firecodev <59217572+firecodev@users.noreply.github.com>
2021-12-23 17:40:23 +0800

    [Add] Add default accounts and categories.

--- lib/src/helpers/db_helper.dart
+++ lib/src/helpers/db_helper.dart
@@ -2,6 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+import '../icon_set.dart';
+import '../color.dart';
+import '../models/category.dart';
+
 class DBHelper {
   static Future<Database> opendb() async {
     String databasePath = await getDatabasesPath();
@@ -30,6 +34,76 @@ Future<void> _onCreate(Database db, int version) async {
         "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, amount INTEGER NOT NULL, account INTEGER NOT NULL, category INTEGER NOT NULL, date INTEGER NOT NULL, remark TEXT, invoice TEXT)");
     await db.execute("CREATE TABLE transfers ("
         "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, amount INTEGER NOT NULL, src INTEGER NOT NULL, dst INTEGER NOT NULL, date INTEGER NOT NULL, remark TEXT)");
+    await db.insert('accounts', {
+      'name': '錢包',
+      'icon': MyIcons.wallet.icon.codePoint,
+      'iconColor': IconColors.myBlack.color.value,
+    });
+    await db.insert('accounts', {
+      'name': '銀行',
+      'icon': MyIcons.bank.icon.codePoint,
+      'iconColor': IconColors.myBlack.color.value,
+    });
+    await db.insert('categories', {
+      'name': '飲食',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.dining.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '服裝',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.cloth.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '居家',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.home.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '交通',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.train.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '教育',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.school.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '娛樂',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.game.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '醫療',
+      'type': Type.expenses.index,
+      'iconData': MyIcons.health.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '薪水',
+      'type': Type.income.index,
+      'iconData': MyIcons.cash.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '利息',
+      'type': Type.income.index,
+      'iconData': MyIcons.savings.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
+    await db.insert('categories', {
+      'name': '投資',
+      'type': Type.income.index,
+      'iconData': MyIcons.invest.icon.codePoint,
+      'iconColor': IconColors.myOrange.color.value,
+    });
   } catch (err) {
     if (!kReleaseMode) {
       print("create database error: $err");
